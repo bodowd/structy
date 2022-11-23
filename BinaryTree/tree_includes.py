684b18a5bcdb63baa19c68425d3e3a2364119a14
bodowd <odowd.bing@gmail.com>
2022-11-23 17:25:28 +0100

    initial

---
+++ BinaryTree/tree_includes.py
@@ -0,0 +1,146 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_includes(root, target):
+    if root is None:
+        return False
+    if root.val == target:
+        return True
+
+    return tree_includes(root.left, target) or tree_includes(root.right, target)
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+r = tree_includes(a, "e")
+print(r)
+assert r == True
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+r = tree_includes(a, "a")
+print(r)
+assert r == True
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+r = tree_includes(a, "n")
+print(r)
+assert r == False
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+g = Node("g")
+h = Node("h")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+r = tree_includes(a, "f")
+print(r)
+assert r == True
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+g = Node("g")
+h = Node("h")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+r = tree_includes(a, "p")
+print(r)
+assert r == False
+
+
+r = tree_includes(None, "b")
+print(r)
+assert r == False
